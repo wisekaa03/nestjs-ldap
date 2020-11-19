078f854c2cd66d0052c9dd175092cbd595711850
Stanislav V Vyaliy <stas@i-npz.ru>
2020-11-19 10:57:26 +0300

    loginDomain fix

--- lib/ldap.class.ts
+++ lib/ldap.class.ts
@@ -322,7 +322,7 @@ export class LdapDomain extends EventEmitter {
                 return { ...o, [key]: entry.object[k] };
               }, {} as Ldap.SearchEntryObject);
 
-              items.push(object);
+              items.push({ loginDomain: this.domainName, ...object });
 
               if (this.options.includeRaw === true) {
                 items[items.length - 1].raw = (entry.raw as unknown) as string;
@@ -604,7 +604,7 @@ export class LdapDomain extends EventEmitter {
         if (sync) {
           await Promise.allSettled(sync.map(async (u) => this.getGroups({ user: u, loggerContext })));
 
-          return { [this.domainName]: { ...((sync as unknown) as LdapResponseUser[]), loginDomain: this.domainName } };
+          return { [this.domainName]: (sync as unknown) as LdapResponseUser[] };
         }
 
         this.logger.error(`${this.domainName}: Synchronize unknown error.`, {
@@ -654,7 +654,7 @@ export class LdapDomain extends EventEmitter {
     })
       .then((sync) => {
         if (sync) {
-          return { [this.domainName]: { ...((sync as unknown) as LdapResponseGroup[]), loginDomain: this.domainName } };
+          return { [this.domainName]: (sync as unknown) as LdapResponseGroup[] };
         }
 
         this.logger.error(`${this.domainName}: synchronizationGroups: unknown error.`, {
